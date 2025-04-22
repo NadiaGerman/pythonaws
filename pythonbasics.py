c02a6e60751be20b898dff8f3aafffd7cb18de22
Nadezda German <gilgofnadia@gmail.com>
2025-04-22 04:18:13 -0700

    Completed Task 11: FizzBuzz

--- pythonbasics.py
+++ pythonbasics.py
@@ -121,3 +121,16 @@ for char in sentence:
         count += 1
 
 print(f"The number of vowels in your sentence is: {count}")
+
+# Task 11: FizzBuzz
+
+for i in range(1, 51):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+
