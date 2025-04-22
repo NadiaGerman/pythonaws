a5be476a90e688423e5f26ef32d6f91903601aa0
Nadezda German <gilgofnadia@gmail.com>
2025-04-22 04:24:39 -0700

    Completed Task 15: FizzBuzz

--- pythonbasics.py
+++ pythonbasics.py
@@ -165,3 +165,17 @@ if word == word[::-1]:
     print(f"'{word}' is a palindrome ")
 else:
     print(f"'{word}' is not a palindrome ")
+
+
+
+# Task 15: FizzBuzz
+
+for i in range(1, 51):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
