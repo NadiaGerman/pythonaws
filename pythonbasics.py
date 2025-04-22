ef209d92711a9358553e49b5101d64778717100a
Nadezda German <gilgofnadia@gmail.com>
2025-04-22 04:22:30 -0700

    Completed Task 14: Palindrome Checker

--- pythonbasics.py
+++ pythonbasics.py
@@ -157,3 +157,11 @@ else:
 
 
 
+# Task 14: Palindrome Checker
+
+word = input("Enter a word: ").lower()
+
+if word == word[::-1]:
+    print(f"'{word}' is a palindrome ")
+else:
+    print(f"'{word}' is not a palindrome ")
