914e0bfaa71b21ba453a9985f521a6c85b183774
Nadezda German <gilgofnadia@gmail.com>
2025-04-22 04:58:46 -0700

    Task 25: Simple calculator with functions

--- pythonbasics.py
+++ pythonbasics.py
@@ -302,20 +302,20 @@
 # top_students = [name for name, grade in students.items() if grade == highest]
 # print("Top student(s):", ", ".join(top_students))
 
-# Task 24: Prime number checker
+# # Task 24: Prime number checker
 
-def is_prime(n):
-    if n <= 1:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+# def is_prime(n):
+#     if n <= 1:
+#         return False
+#     for i in range(2, int(n ** 0.5) + 1):
+#         if n % i == 0:
+#             return False
+#     return True
 
-# Check numbers from 1 to 20
-for num in range(1, 21):
-    if is_prime(num):
-        print(f"{num} is prime")
-    else:
-        print(f"{num} is not prime")
+# # Check numbers from 1 to 20
+# for num in range(1, 21):
+#     if is_prime(num):
+#         print(f"{num} is prime")
+#     else:
+#         print(f"{num} is not prime")
 
