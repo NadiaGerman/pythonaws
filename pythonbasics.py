4202728b0092072287192c1d6241a45f21b37fcc
Nadezda German <gilgofnadia@gmail.com>
2025-04-22 04:21:06 -0700

    Completed Task 13: Factorial Calculator

--- pythonbasics.py
+++ pythonbasics.py
@@ -143,3 +143,17 @@ if word == word[::-1]:
 else:
     print(f"'{word}' is not a palindrome.")
 
+    # Task 13: Factorial Calculator
+
+num = int(input("Enter a positive integer: "))
+
+if num < 0:
+    print("Factorial is not defined for negative numbers.")
+else:
+    factorial = 1
+    for i in range(1, num + 1):
+        factorial *= i
+    print(f"The factorial of {num} is {factorial}")
+
+
+
