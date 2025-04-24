374e0c002d74d39c95aa5270f3165c247bf67048
Nadezda German <gilgofnadia@gmail.com>
2025-04-23 23:12:26 -0700

    Exercise 32: Password Strenth Checker

---
+++ pithon2les.py
@@ -0,0 +1,12 @@
+first_place = "Alice"
+second_place = "Bob"
+third_place = "Charlie"
+fourth_place = "Dana"
+fifth_place = "Eli"
+
+print("🏁 Race Results:")
+print("1st Place:", first_place)
+print("2nd Place:", second_place)
+print("3rd Place:", third_place)
+print("4th Place:", fourth_place)
+print("5th Place:", fifth_place)
