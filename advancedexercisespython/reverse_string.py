56fe636ac6b91fe883eba86e979090707a579039
Nadezda German <gilgofnadia@gmail.com>
2025-04-27 01:25:03 -0700

    Add reverse string function

---
+++ advancedexercisespython/reverse_string.py
@@ -0,0 +1,12 @@
+# reverse_string.py
+
+def reverse_string(text):
+    """Reverse the given string."""
+    return text[::-1]
+
+# Testing the function
+print(reverse_string("hello"))
+print(reverse_string("Python is fun!"))
+print(reverse_string("12345"))
+print(reverse_string("A"))
+print(reverse_string(""))
